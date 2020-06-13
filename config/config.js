de628910c8a643e38ddfcfcfc8a5b59b037792d1
vagga ravi <vaggaravi@gmail.com>
2020-06-13 17:02:24 +0530

    initial commit

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// config.js
+const config = {
+   
+    db: {
+      host: 'localhost',
+      dbName: 'trackingsystem',
+      user:'vagga',
+      password:'Vagga@123!'
+    }
+   };
+   
+   module.exports = config;
