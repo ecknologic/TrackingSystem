3547a51e90f72246622ddb541c53af10b59e898a
Ecknologic <spasupuleti@ecknologic.com>
2020-07-26 12:52:47 +0530

    Mother plant APIS

--- routes/motherPlant.js
+++ routes/motherPlant.js
@@ -9,19 +9,6 @@ router.use(function timeLog(req, res, next) {
 });
 
 
-/**
-* @swagger
-* /motherPlant/getQualityControl:
-*   get:
-*     tags:
-*       - Warehouse
-*     description: Getting Quality control details
-*     produces:
-*       - application/json
-*     responses:
-*       200:
-*         description: Successfully 
-*/
 
 router.get('/getQualityControl', (req, res) => {
     let query = "select * from qualitycontrol";
@@ -54,19 +41,6 @@ router.post('/createQC', (req, res) => {
     });
 })
 
-/**
-* @swagger
-* /motherPlant/getRMDetails:
-*   get:
-*     tags:
-*       - Warehouse
-*     description: Getting Required Raw Materials details
-*     produces:
-*       - application/json
-*     responses:
-*       200:
-*         description: Successfully 
-*/
 
 router.get('/getRMDetails', (req, res) => {
     let query = "select * from requiredrawmaterial";
@@ -87,6 +61,94 @@ router.post('/createRM', (req, res) => {
     let input = req.body;
 
     let insertQueryValues = [input.itemName, input.description, input.recordLevel]
+    db.query(CreateQuery, insertQueryValues, (err, results) => {
+
+        console.log(insertQueryValues);
+
+        if (err) throw err;
+        else
+            res.send("Record Inserted");
+
+    });
+})
+router.get('/getRMReceipt', (req, res) => {
+    let query = "select * from rawmaterialreceipt";
+    let result = db.query(query, (err, results) => {
+
+        if (err) throw err;
+
+        res.send(JSON.stringify(results));
+
+
+    });
+});
+router.post('/createRMReceipt', (req, res) => {
+    let CreateQuery = "insert into rawmaterialreceipt (receiptdate,receivedFromParty,invoiceNo,itemreceived,price,qtyReceived,tax,invoiceValue,rawmaterialId) values(?,?,?,?,?,?,?,?,?)";
+
+    console.log(req.body);
+
+    let input = req.body;
+
+    let insertQueryValues = [input.receiptdate, input.receivedFromParty, input.invoiceNo, input.itemreceived, input.price, input.qtyReceived, input.tax, input.invoiceValue, input.rawmaterialId]
+    db.query(CreateQuery, insertQueryValues, (err, results) => {
+
+        console.log(insertQueryValues);
+
+        if (err) throw err;
+        else
+            res.send("Record Inserted");
+
+    });
+})
+router.get('/getDispatchDetails', (req, res) => {
+    let query = "select * from dispatches";
+    let result = db.query(query, (err, results) => {
+
+        if (err) throw err;
+
+        res.send(JSON.stringify(results));
+
+
+    });
+});
+router.post('/addDispatchDetails', (req, res) => {
+    let CreateQuery = "insert into dispatches (dispatchId,dispatchedDate,DCNO,vehicleNo,driverName,dispatchTo,itemDispatched,qtyDispatched) values(?,?,?,?,?,?,?,?)";
+
+    console.log(req.body);
+
+    let input = req.body;
+
+    let insertQueryValues = [input.dispatchId, input.dispatchedDate, input.DCNO, input.vehicleNo, input.driverName, input.dispatchTo, input.itemDispatched, input.qtyDispatched]
+    db.query(CreateQuery, insertQueryValues, (err, results) => {
+
+        console.log(insertQueryValues);
+
+        if (err) throw err;
+        else
+            res.send("Record Inserted");
+
+    });
+})
+router.get('/getProductionDetails', (req, res) => {
+    console.log("GEGEGEG")
+    let query = "select * from production";
+    let result = db.query(query, (err, results) => {
+
+        if (err) throw err;
+
+        res.send(JSON.stringify(results));
+
+
+    });
+});
+router.post('/addProductionDetails', (req, res) => {
+    let CreateQuery = "insert into production (productionDate,batchNo,phLevel,TDS,ozonelevel,qtyproduced,itemproduced) values(?,?,?,?,?,?,?)";
+
+    console.log(req.body);
+
+    let input = req.body;
+
+    let insertQueryValues = [input.productionDate, input.batchNo, input.phLevel, input.TDS, input.ozonelevel, input.qtyproduced, input.itemproduced]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
 
         console.log(insertQueryValues);
