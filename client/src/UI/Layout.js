0e881b1b1f15655c09c8ceba06b572257fed148a
Ecknologic <spasupuleti@ecknologic.com>
2020-10-29 19:50:53 +0530

    small change

--- client/src/UI/Layout.js
+++ client/src/UI/Layout.js
@@ -33,11 +33,11 @@ const LayoutPage = (props) => {
                             defaultOpenKeys={['sub1']}
                             style={{ height: '100%', borderRight: 0 }}
                         >
-                            <Menu.Item key="1" key="sub1"><Link to="/bibowarehouse"><span></span> <span>Dashboard</span></Link></Menu.Item>
+                            <Menu.Item key="1" key="sub1"><Link to={ROLE == WAREHOUSEADMIN ? "/bibowarehouse" : "/addcustomer"}><span></span> <span>Dashboard</span></Link></Menu.Item>
                             {ROLE == WAREHOUSEADMIN ? <Menu.Item key="2" key="sub2"><span><img src={dashboardIcon12} alt="contest-cover" /></span> <span>Manage Stock</span></Menu.Item> : null}
                             <Menu.Item key="3" key="sub3"><span><img src={dashboardIcon} alt="contest-cover" /></span> <span>Manage Routes</span></Menu.Item>
                             {ROLE == MARKETINGADMIN ? <Menu.Item key="4" key="sub4"><Link to="/addcustomer"><span><img src={dashboardIcon} alt="contest-cover" /></span><span>Add Customer</span></Link></Menu.Item> : null}
-                            <Menu.Item key="5" key="sub5"><Link to="/manageaccount"><span><img src={dashboardIcon12} alt="contest-cover" /></span> <span>Manage Stock</span></Link></Menu.Item>
+                            {/* <Menu.Item key="5" key="sub5"><Link to="/manageaccount"><span><img src={dashboardIcon12} alt="contest-cover" /></span> <span>Manage Stock</span></Link></Menu.Item> */}
                             <Menu.Item key="6" key="sub6"><span></span> <span>Reports</span></Menu.Item>
                             {/* <SubMenu key="sub1" icon={<UserOutlined />} title="Dashboard">
                                 <Menu.Item key="1">option1</Menu.Item>
