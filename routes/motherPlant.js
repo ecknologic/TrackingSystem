399fd6511cf178ae0efde3fdff932838e536b2dc
Ecknologic <spasupuleti@ecknologic.com>
2020-12-10 10:52:08 +0530

    Added extra keys in Motherplant

--- routes/motherPlant.js
+++ routes/motherPlant.js
@@ -23,182 +23,135 @@ router.get('/getQualityControl', (req, res) => {
 });
 
 router.post('/createQC', (req, res) => {
-    let CreateQuery = "insert into qualitycontrol (reportdate,batchNo,testType,reportImage) values(?,?,?,?)";
-
-    console.log(req.body);
-
+    let CreateQuery = "insert into qualitycontrol (reportdate,batchNo,testType,reportImage,description) values(?,?,?,?,?)";
     let input = req.body;
-
-    let insertQueryValues = [input.reportdate, input.batchNo, input.testType, input.reportImage]
+    let reportImage = Buffer.from(input.reportImage.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+    let insertQueryValues = [input.reportdate, input.batchNo, input.testType, reportImage, input.description]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
-
         console.log(insertQueryValues);
-
         if (err) res.send(err);
         else
             res.send("Record Inserted");
-
     });
 })
-
+router.get('/getInternalQualityControl', (req, res) => {
+    let query = "select * from internalqualitycontrol";
+    db.query(query, (err, results) => {
+        if (err) res.send(err);
+        res.json(results);
+    });
+});
+router.post('/createInternalQC', (req, res) => {
+    let CreateQuery = "insert into internalqualitycontrol (productionDate,batchNo,testType,description) values(?,?,?,?,?)";
+    let input = req.body;
+    let insertQueryValues = [input.productionDate, input.batchNo, input.testType, input.description]
+    db.query(CreateQuery, insertQueryValues, (err, results) => {
+        if (err) res.send(err);
+        else
+            res.send("Record Inserted");
+    });
+})
 
 router.get('/getRMDetails', (req, res) => {
     let query = "select * from requiredrawmaterial";
     let result = db.query(query, (err, results) => {
-
         if (err) res.send(err);
-
         res.send(JSON.stringify(results));
-
-
     });
 });
+
 router.post('/createRM', (req, res) => {
-    let CreateQuery = "insert into requiredrawmaterial (itemName,description,recordLevel) values(?,?,?)";
-
-    console.log(req.body);
-
+    let CreateQuery = "insert into requiredrawmaterial (itemName,description,recordLevel,minOrderLevel) values(?,?,?,?)";
     let input = req.body;
 
-    let insertQueryValues = [input.itemName, input.description, input.recordLevel]
+    let insertQueryValues = [input.itemName, input.description, input.recordLevel, input.minOrderLevel]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
-
-        console.log(insertQueryValues);
-
         if (err) res.send(err);
         else
             res.send("Record Inserted");
-
     });
 })
 router.get('/getRMReceipt', (req, res) => {
     let query = "select * from rawmaterialreceipt";
-    let result = db.query(query, (err, results) => {
-
+    db.query(query, (err, results) => {
         if (err) res.send(err);
-
         res.send(JSON.stringify(results));
-
-
     });
 });
+
 router.post('/createRMReceipt', (req, res) => {
-    let CreateQuery = "insert into rawmaterialreceipt (receiptdate,receivedFromParty,invoiceNo,itemreceived,price,qtyReceived,tax,invoiceValue,rawmaterialId) values(?,?,?,?,?,?,?,?,?)";
-
-    console.log(req.body);
-
+    let CreateQuery = "insert into rawmaterialreceipt (receiptdate,receivedFromParty,invoiceNo,itemreceived,price,qtyReceived,tax,invoiceValue,rawmaterialId,invoiceDate) values(?,?,?,?,?,?,?,?,?,?)";
     let input = req.body;
-
-    let insertQueryValues = [input.receiptdate, input.receivedFromParty, input.invoiceNo, input.itemreceived, input.price, input.qtyReceived, input.tax, input.invoiceValue, input.rawmaterialId]
+    let insertQueryValues = [input.receiptdate, input.receivedFromParty, input.invoiceNo, input.itemreceived, input.price, input.qtyReceived, input.tax, input.invoiceValue, input.rawmaterialId, input.invoiceDate]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
-
-        console.log(insertQueryValues);
-
         if (err) res.send(err);
         else
             res.send("Record Inserted");
-
     });
 })
+
 router.get('/getDispatchDetails', (req, res) => {
     let query = "select * from dispatches";
-    let result = db.query(query, (err, results) => {
-
+     db.query(query, (err, results) => {
         if (err) res.send(err);
-
         res.send(JSON.stringify(results));
-
-
     });
 });
+
 router.post('/addDispatchDetails', (req, res) => {
-    let CreateQuery = "insert into dispatches (dispatchId,dispatchedDate,DCNO,vehicleNo,driverName,dispatchTo,itemDispatched,qtyDispatched) values(?,?,?,?,?,?,?,?)";
-
-    console.log(req.body);
-
+    let CreateQuery = "insert into dispatches (dispatchId,dispatchedDate,DCNO,vehicleNo,driverName,dispatchTo,itemDispatched,qtyDispatched,batchNo) values(?,?,?,?,?,?,?,?,?)";
     let input = req.body;
-
-    let insertQueryValues = [input.dispatchId, input.dispatchedDate, input.DCNO, input.vehicleNo, input.driverName, input.dispatchTo, input.itemDispatched, input.qtyDispatched]
+    let insertQueryValues = [input.dispatchId, input.dispatchedDate, input.DCNO, input.vehicleNo, input.driverName, input.dispatchTo, input.itemDispatched, input.qtyDispatched, input.batchNo]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
-
-        console.log(insertQueryValues);
-
         if (err) res.send(err);
         else
             res.send("Record Inserted");
-
     });
 })
+
 router.get('/getProductionDetails', (req, res) => {
     let query = "select * from production";
-    let result = db.query(query, (err, results) => {
-
+ db.query(query, (err, results) => {
         if (err) res.send(err);
-
         res.send(JSON.stringify(results));
-
-
     });
 });
 
 router.get('/getNatureOfBussiness', (req, res) => {
- 
     let query = "SELECT SUBSTRING(COLUMN_TYPE,5) AS natureOfBussiness FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'customerorderdetails' AND COLUMN_NAME = 'isDelivered'";
-    let result = db.query(query, (err, results) => {
-  
-      if (err) res.send(err);
-      res.send(JSON.stringify(results));
-  
-  
+     db.query(query, (err, results) => {
+        if (err) res.send(err);
+        res.send(JSON.stringify(results));
     });
-  });
+});
 
 router.get('/getVehicleDetails', (req, res) => {
     let query = "select * from vehicleDetails";
-    let result = db.query(query, (err, results) => {
-
+     db.query(query, (err, results) => {
         if (err) res.send(err);
-
         res.send(JSON.stringify(results));
-
-
     });
 });
 
 router.post('/addVehicleDetails', (req, res) => {
     let CreateQuery = "insert into VehicleDetails (vehicleNo,vehicleType) values(?,?)";
-
-    console.log(req.body);
-
     let input = req.body;
-
     let insertQueryValues = [input.vehicleNo, input.vehicleType]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
-
-        console.log(insertQueryValues);
-
         if (err) res.send(err);
         else
             res.send("Record Inserted");
-
     });
 })
+
 router.post('/addProductionDetails', (req, res) => {
     let CreateQuery = "insert into production (productionDate,batchNo,phLevel,TDS,ozonelevel,qtyproduced,itemproduced) values(?,?,?,?,?,?,?)";
-
-    console.log(req.body);
-
     let input = req.body;
-
     let insertQueryValues = [input.productionDate, input.batchNo, input.phLevel, input.TDS, input.ozonelevel, input.qtyproduced, input.itemproduced]
     db.query(CreateQuery, insertQueryValues, (err, results) => {
-
-        console.log(insertQueryValues);
-
         if (err) res.send(err);
         else
             res.send("Record Inserted");
-
     });
 })
 module.exports = router;
