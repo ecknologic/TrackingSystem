d689bafde2feaad852cd7d22a89c6005d1a67a62
Ecknologic <spasupuleti@ecknologic.com>
2020-12-30 23:37:16 +0530

    Customer Approval APIs

--- dbQueries/Customer/queries.js
+++ dbQueries/Customer/queries.js
@@ -5,17 +5,37 @@ customerQueries.getCustomerDetails = (customerId, callback) => {
     let query = "SELECT customerId,customerName,mobileNumber,EmailId,Address1,gstNo,panNo,adharNo,registeredDate,invoicetype,natureOfBussiness,creditPeriodInDays,referredBy,isActive,customertype,organizationName,idProofType,customer_id_proof,d.idProof_backside,d.idProof_frontside,d.gstProof from customerdetails c INNER JOIN customerDocStore d ON c.customer_id_proof=d.docId WHERE c.customerId=" + customerId
     executeGetQuery(query, callback)
 }
+customerQueries.getCustomersByCustomerType = (customerType, callback) => {
+    let query = "SELECT c.organizationName,c.isActive,c.customerId,c.natureOfBussiness,c.customerName,c.registeredDate,c.address1 AS address,JSON_ARRAYAGG(d.contactperson) AS contactpersons FROM customerdetails c INNER JOIN DeliveryDetails d ON c.customerId=d.customer_Id WHERE c.customertype=?  GROUP BY c.organizationName,c.customerName,c.natureOfBussiness,c.address1,c.isActive,c.customerId,c.registeredDate ORDER BY c.registeredDate DESC"
+    executeGetParamsQuery(query, [customerType], callback)
+}
+customerQueries.getCustomerDetailsByStatus = (status, callback) => {
+    let query = "SELECT c.organizationName,c.isActive,c.customerId,c.natureOfBussiness,c.customerName,c.registeredDate,c.address1 AS address,JSON_ARRAYAGG(d.contactperson) AS contactpersons FROM customerdetails c INNER JOIN DeliveryDetails d ON c.customerId=d.customer_Id WHERE c.isApproved=?  GROUP BY c.organizationName,c.customerName,c.natureOfBussiness,c.address1,c.isActive,c.customerId,c.registeredDate ORDER BY c.registeredDate DESC"
+    console.log(query,status)
+    executeGetParamsQuery(query, [status], callback)
+}
 customerQueries.getsqlNo = (tableName, callback) => {
     let seqNoQuery = `SELECT AUTO_INCREMENT AS orderId FROM information_schema.TABLES WHERE TABLE_NAME=?`
     executeGetParamsQuery(seqNoQuery, [tableName], callback)
 }
 //POST Request Methods
 customerQueries.saveCustomerOrderDetails = (input, callback) => {
-    let { customerName, phoneNumber, address, routeId, driverId, customer_Id, latitude, longitude, dcNo, departmentId, customerType, product20L, price20L, product1L, price1L, product500ML, price500ML,
+    let { contactPerson, phoneNumber, address, routeId, driverId, customer_Id, latitude, longitude, dcNo, departmentId, customerType, product20L, price20L, product1L, price1L, product500ML, price500ML,
         product250ML, price250ML } = input
     let query = `insert into customerorderdetails (customerName,phoneNumber,address,routeId,driverId,existingCustomerId,latitude,longitude,dcNo,warehouseId,customerType,20LCans, price20L, 1LBoxes, price1L, 500MLBoxes, price500ML,250MLBoxes, price250ML) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
-    let requestBody = [customerName, phoneNumber, address, routeId, driverId, customer_Id, latitude, longitude, dcNo, departmentId, customerType, product20L, price20L, product1L, price1L, product500ML, price500ML, product250ML, price250ML]
+    let requestBody = [contactPerson, phoneNumber, address, routeId, driverId, customer_Id, latitude, longitude, dcNo, departmentId, customerType, product20L, price20L, product1L, price1L, product500ML, price500ML, product250ML, price250ML]
     executePostOrUpdateQuery(query, requestBody, callback)
 }
-
+customerQueries.approveCustomer = (customerId, callback) => {
+    let query = `UPDATE customerdetails set isApproved=1 where customerId=?`
+    executePostOrUpdateQuery(query, [customerId], callback)
+}
+customerQueries.approveDeliveryDetails = (ids, callback) => {
+    let query = 'UPDATE DeliveryDetails set isActive=1 where deliveryDetailsId IN (?)'
+    executePostOrUpdateQuery(query, [ids], callback)
+}
+customerQueries.deleteDeliveryAddress = (deliveryId, callback) => {
+    let query = "update DeliveryDetails set deleted=1 where deliveryDetailsId=?"
+    executePostOrUpdateQuery(query, [deliveryId], callback)
+}
 module.exports = customerQueries
--- routes/utilities.js
+++ routes/utilities.js
@@ -20,67 +20,67 @@ const insertToCustomerOrderDetails = async (result, res) => {
     if (err) console.log(err)
     else {
       seqNo = results[0].orderId;
-          // let { customerName, phoneNumber, address, routeId, driverId, customer_Id, latitude, longitude } = result      //uncomment
-          let requestBody = result
-          // const { customerName, phoneNumber, address, routeId = 0, driverId = 0, existingCustomerId, latitude, longitude } = customerData[0]
-          requestBody.dcNo = "DC-" + seqNo
-          requestBody.departmentId = result.departmentId || 1
-          requestBody.customerType = 'Internal'
-          requestBody.phoneNumber = "9985758537"
-          customerProductDetails(deliveryDetailsId).then(products => {
-            products.map(product => {
-              if (product.productName == '20L') {
-                requestBody.product20L = product.noOfJarsTobePlaced
-                requestBody.price20L = product.productPrice
-              } else if (product.productName == '1L') {
-                requestBody.product1L = product.noOfJarsTobePlaced
-                requestBody.price1L = product.productPrice
-              } else if (product.productName == '500ML') {
-                requestBody.product500ML = product.noOfJarsTobePlaced
-                requestBody.price500ML = product.productPrice
-              } else if (product.productName == '250ML') {
-                requestBody.product250ML = product.noOfJarsTobePlaced
-                requestBody.price250ML = product.productPrice
-              }
-            })
-            customerQueries.saveCustomerOrderDetails(requestBody, (err, results) => {
-              if (err) throw err;
-              else {
-                /*   let updateQuery="update orderdetails set transactionid=? where orderid=?"
-                db.query(updateQuery,[warehouseName+results.insertId,results.insertId],(err,results)=>{
-                    if(err) throw err;
-                    else 
-                });*/
-                console.log('record inserted');
-              }
-            });
-          }).catch(err => {
-            console.log(err)
-          })
+      let requestBody = result
+      requestBody.dcNo = "DC-" + seqNo
+      requestBody.departmentId = result.departmentId || 1
+      requestBody.customerType = 'Internal'
+      requestBody.phoneNumber = "9985758537"
+      customerProductDetails(deliveryDetailsId).then(products => {
+        products.map(product => {
+          if (product.productName == '20L') {
+            requestBody.product20L = product.noOfJarsTobePlaced
+            requestBody.price20L = product.productPrice
+          } else if (product.productName == '1L') {
+            requestBody.product1L = product.noOfJarsTobePlaced
+            requestBody.price1L = product.productPrice
+          } else if (product.productName == '500ML') {
+            requestBody.product500ML = product.noOfJarsTobePlaced
+            requestBody.price500ML = product.productPrice
+          } else if (product.productName == '250ML') {
+            requestBody.product250ML = product.noOfJarsTobePlaced
+            requestBody.price250ML = product.productPrice
+          }
+        })
+        customerQueries.saveCustomerOrderDetails(requestBody, (err, results) => {
+          if (err) throw err;
+          else {
+            /*   let updateQuery="update orderdetails set transactionid=? where orderid=?"
+            db.query(updateQuery,[warehouseName+results.insertId,results.insertId],(err,results)=>{
+                if(err) throw err;
+                else 
+            });*/
+            console.log('record inserted');
+          }
+        });
+      }).catch(err => {
+        console.log(err)
+      })
     }
   });
 }
 //Scheduling the 
 cron.schedule('0 0 2 * * *', function () {
+  saveToCustomerOrderDetails()
+});
+
+function saveToCustomerOrderDetails(customerId) {
   var day = days[new Date().getDay()];
 
-  let customerDeliveryDaysQuery = "SELECT c.deliveryDetailsId,c.customer_Id,c.phoneNumber,c.address,c.contactPerson,c.departmentId,c.routeId,c.driverId,c.latitude,c.longitude FROM DeliveryDetails c INNER JOIN  customerdeliverydays cd ON cd.deliveryDaysId=c.deliverydaysid  WHERE " + day + "=1";
-
+  let customerDeliveryDaysQuery = "SELECT c.deliveryDetailsId,c.customer_Id,c.phoneNumber,c.address,c.contactPerson,c.departmentId,c.routeId,c.driverId,c.latitude,c.longitude FROM DeliveryDetails c INNER JOIN  customerdeliverydays cd ON cd.deliveryDaysId=c.deliverydaysid  WHERE c.deleted=0 AND c.isActive=1 AND " + day + "=1";
+  if (customerId) {
+    customerDeliveryDaysQuery += ' AND customer_Id=' + customerId
+  }
   db.query(customerDeliveryDaysQuery, (err, results) => {
-
     if (err) throw err;
-
     if (results.length > 0) {
       results.forEach(result => {
         // var warehouseName=result.shortName+"-";
         insertToCustomerOrderDetails(result)
       });
-
     }
   });
-});
+}
 
 
-
-
+module.exports = { saveToCustomerOrderDetails }
 // module.exports = router;
