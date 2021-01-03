e2578f3536a257b86f02a3dd8e7444e1a4d41212
Dilip Reddy <spasupuleti@ecknologic.com>
2021-01-03 15:24:19 +0530

    add dropdown in address card

--- client/src/components/AddressCard.js
+++ client/src/components/AddressCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Dropdown, Menu } from 'antd';
 import NameCard from './NameCard';
 import PrimaryButton from './PrimaryButton';
-import { FriendIconGrey } from './SVG_Icons';
+import { FriendIconGrey, MoreIconGrey } from './SVG_Icons';
 import '../sass/accountCard.scss'
 import '../sass/addressCard.scss'
 
 const AddressCard = ({ data, onClick }) => {
     const { isActive, departmentName, phoneNumber, location, contactPerson } = data
+
+    const onSelect = (value) => {
+    }
 
     return (
         <div className='account-card-container address-card-container'>
@@ -34,10 +38,34 @@ const AddressCard = ({ data, onClick }) => {
             </div>
             <div className='footer'>
                 <PrimaryButton text='View Details' onClick={() => onClick(data)} />
+                <Action onSelect={onSelect} />
             </div>
         </div>
     )
 
+}
+
+const Action = ({ onSelect }) => {
+    const menu = (
+        <Menu onClick={onSelect}>
+            <Menu.Item key="approve">
+                Approve
+          </Menu.Item>
+            <Menu.Item key="delete">
+                Delete
+          </Menu.Item>
+        </Menu>
+    );
+
+    return (
+        <Dropdown
+            overlay={menu}
+            trigger={['click']}
+            getPopupContainer={() => document.getElementById('content')}
+        >
+            <MoreIconGrey className='action-dots' />
+        </Dropdown>
+    )
 }
 
 export default AddressCard
