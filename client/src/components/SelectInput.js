60341b7b5378e8df5674e040743a6f00d5077fa3
Dilip Reddy <spasupuleti@ecknologic.com>
2021-01-05 12:04:50 +0530

    using only one primary color for blue variants

--- client/src/components/SelectInput.js
+++ client/src/components/SelectInput.js
@@ -30,7 +30,7 @@ const SelectInput = ({ options, mode, onSelect, onDeselect, value, disabled,
 
         if (label === 'ALL') return null
         return (
-            <Tag color='#005FFF' closable={closable} onClose={onClose} style={{ marginRight: 3 }}>
+            <Tag color='#0062FF' closable={closable} onClose={onClose} style={{ marginRight: 3 }}>
                 {label}
             </Tag>
         );
