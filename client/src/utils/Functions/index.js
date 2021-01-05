b648eb866a18e59978f329ebd984c550e951e2ff
Dilip Reddy <spasupuleti@ecknologic.com>
2021-01-05 21:44:22 +0530

    driver api issue fixed

--- client/src/utils/Functions/index.js
+++ client/src/utils/Functions/index.js
@@ -442,6 +442,9 @@ export const getStatusColor = (status) => {
         case 'Delivered':
             return '#0EDD4D'
 
+        case 'Completed':
+            return '#0EDD4D'
+
         case 'Approved':
             return '#0EDD4D'
 
