a7b3be07bb95ff7a5e0fa2f2e8350ef794187451
praveen <praveen14568@gmail.com>
2021-01-15 23:54:19 +0530

    userId added for dependent details

--- dbQueries/driver/queries.js
+++ dbQueries/driver/queries.js
@@ -6,7 +6,7 @@ driverQueries.getDrivers = async (callback) => {
     return executeGetQuery(query, callback)
 }
 driverQueries.getDriverById = async (driverId, callback) => {
-    let query = "SELECT d.*,d.driverName as userName,s.adharNo as dependentAdharNo,s.adhar_frontside as dependentFrontProof,s.adhar_backside as dependentBackProof,JSON_OBJECT('name',s.name,'dob',s.dob,'gender',s.gender,'mobileNumber',s.mobileNumber,'relation',s.relation,'dependentId',s.dependentId) dependentDetails from driverdetails d INNER JOIN driverDependentDetails s on d.driverId=s.userId where d.driverId=" + driverId;
+    let query = "SELECT d.*,d.driverName as userName,s.adharNo as dependentAdharNo,s.adhar_frontside as dependentFrontProof,s.adhar_backside as dependentBackProof,JSON_OBJECT('name',s.name,'userId',s.userId,'dob',s.dob,'gender',s.gender,'mobileNumber',s.mobileNumber,'relation',s.relation,'dependentId',s.dependentId) dependentDetails from driverdetails d INNER JOIN driverDependentDetails s on d.driverId=s.userId where d.driverId=" + driverId;
     return executeGetQuery(query, callback)
 }
 
