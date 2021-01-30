4a762ae1e16b0abf2c1785ce77027709e8d4853a
Dilip Reddy <spasupuleti@ecknologic.com>
2021-01-30 17:34:36 +0530

    add time in notification card

--- client/src/components/NotificationCard.js
+++ client/src/components/NotificationCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../sass/notificationCard.scss'
 
-const NotificationCard = ({ title, content }) => {
+const NotificationCard = ({ time, title, content }) => {
 
     return (
         <div className='notification-card'>
+            <div className='time'>{time}</div>
             <div className='header'>{title}</div>
             <div className='body'>{content}</div>
         </div>
