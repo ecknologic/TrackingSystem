b4516882462cda78e0f3f820ddc5717126aa140b
praveen <praveen14568@gmail.com>
2021-02-22 22:32:51 +0530

    billing address fixed

--- routes/customer.js
+++ routes/customer.js
@@ -663,7 +663,7 @@ router.get("/getCustomerBillingAddress/:customerId", (req, res) => {
     else {
       if (data.length) {
         let result = data[0]
-        result.isLocal = result.gstNo.startsWith('36')
+        result.isLocal = result.gstNo ? result.gstNo.startsWith('36') : false
         result.emailId = `${result.customerName} <${result.EmailId}>`
         delete result.EmailId
         res.json(result)
