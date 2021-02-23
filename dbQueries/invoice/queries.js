ddc8274101f332b9341d86afb3d535e308af5e34
praveen <praveen14568@gmail.com>
2021-02-23 20:16:44 +0530

    small changes

--- dbQueries/invoice/queries.js
+++ dbQueries/invoice/queries.js
@@ -13,23 +13,19 @@ invoiceQueries.getInvoiceId = async (callback) => {
 
 //POST Request Methods
 invoiceQueries.createInvoice = (input, callback) => {
-    const { customerId, invoiceDate, dueDate, salesPerson, mailSubject, mailIds, tAndc, invoicePdf } = input
-    let query = "insert into Invoice (customerId,invoiceDate,dueDate,salesPerson,mailSubject,mailIds,tAndc,invoicePdf) values(?,?,?,?,?,?,?)";
+    const { customerId, invoiceDate, dueDate, salesPerson, mailSubject, mailIds, tAndc, invoicePdf, invoiceNumber, hsnCode, poNo } = input
+    let query = "insert into Invoice (customerId,invoiceDate,dueDate,salesPerson,mailSubject,mailIds,tAndc,invoicePdf,invoiceId,hsnCode,poNo) values(?,?,?,?,?,?,?,?,?,?,?)";
     // var gstProofImage = Buffer.from(gstProof.replace(/^data:image\/\w+;base64,/, ""), 'base64')
-    let requestBody = [customerId, invoiceDate, dueDate, salesPerson, mailSubject, mailIds, tAndc, invoicePdf]
+    let requestBody = [customerId, invoiceDate, dueDate, salesPerson, mailSubject, mailIds, tAndc, invoicePdf, invoiceNumber, hsnCode, poNo]
     executePostOrUpdateQuery(query, requestBody, callback)
 }
 invoiceQueries.saveInvoiceProducts = (input, callback) => {
     const { invoiceId = 1, products } = input
     // const sql = products.map(item => `(${invoiceId},${String(item.productName)}, ${item.productPrice}, ${item.discount}, ${item.quantity}, ${item.tax})`)
-    const sql = products.map(item => "(" + invoiceId + ", '" + item.productName + "', " + item.productPrice + ", " + item.discount + ", " + item.quantity + ", " + item.tax + ")")
+    const sql = products.map(item => "('" + invoiceId + "', '" + item.productName + "', " + item.productPrice + ", " + item.discount + ", " + item.quantity + ", " + item.tax + ")")
     let query = "insert into invoiceProductsDetails (invoiceId, productName, productPrice, discount, quantity, tax) values " + sql;
     // var gstProofImage = Buffer.from(gstProof.replace(/^data:image\/\w+;base64,/, ""), 'base64')
     // let requestBody = [invoiceId, productName, productPrice, discount, quantity, tax]
-    console.log(query)
-    executeGetQuery(query, (err, data) => {
-        if (err) console.log("ddd", err)
-        else console.log("Data", data)
-    })
+    executeGetQuery(query, callback)
 }
 module.exports = invoiceQueries
