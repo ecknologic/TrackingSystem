99b9a017812685072b62f8fb2ed13686269b67e8
praveen <praveen14568@gmail.com>
2021-03-05 21:08:35 +0530

    Fixed motherplant count issue

--- dbQueries/motherplant/queries.js
+++ dbQueries/motherplant/queries.js
@@ -270,7 +270,8 @@ motherPlantDbQueries.getPDDetailsByDate = async (input, callback) => {
     return executeGetParamsQuery(query, [input.departmentId, input.date], callback)
 }
 motherPlantDbQueries.getTotalPDDetails = async (input, callback) => {
-    let { departmentId, startDate, endDate, fromStart, batchIds } = input;
+    let { departmentId, startDate, endDate, fromStart, batchIds: bIds } = input;
+    const batchIds = bIds.split(',')
     let options = [departmentId, endDate, batchIds]
     let query = "SELECT SUM(p.product20L) AS total20LCans,SUM(p.product1L) AS total1LBoxes,SUM(p.product500ML) total500MLBoxes,SUM(p.product300ML) total300MLBoxes,SUM(p.product2L) total2LBoxes FROM dispatches p WHERE departmentId=? AND DATE(`dispatchedDate`)<=? AND batchId IN (?)";
 
