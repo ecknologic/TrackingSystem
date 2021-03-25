4a83a21a95be6fa387ecdeb154f32c226b527c63
Dilip Reddy <spasupuleti@ecknologic.com>
2021-03-25 21:34:57 +0530

    changes

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -121,9 +121,9 @@ router.post('/updateWarehouse', (req, res) => {
 });
 
 router.post('/createDC', (req, res) => {
-  let dcCreateQuery = "insert into customerorderdetails (customerName,phoneNumber,address,routeId,driverId,20LCans,1LBoxes,500MLBoxes,300MLBoxes,2LBoxes,warehouseId,customerType,existingCustomerId) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-  let dcDetails = req.body;
-  let insertQueryValues = [dcDetails.customerName, dcDetails.phoneNumber, dcDetails.address, dcDetails.routeId, dcDetails.driverId, dcDetails.product20L, dcDetails.product1L, dcDetails.product500ML, dcDetails.product300ML, dcDetails.product2L, dcDetails.warehouseId, dcDetails.customerType, dcDetails.existingCustomerId]
+  let dcCreateQuery = "insert into customerorderdetails (customerName,phoneNumber,address,routeId,driverId,20LCans,1LBoxes,500MLBoxes,300MLBoxes,2LBoxes,warehouseId,customerType,existingCustomerId,creationType,isDelivered) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  let { customerName, phoneNumber, address, routeId, driverId, product20L, product1L, product500ML, product300ML, product2L, warehouseId, customerType, existingCustomerId, creationType, isDelivered = 'InProgress' } = req.body;
+  let insertQueryValues = [customerName, phoneNumber, address, routeId, driverId, product20L, product1L, product500ML, product300ML, product2L, warehouseId, customerType, existingCustomerId, creationType, isDelivered]
   db.query(dcCreateQuery, insertQueryValues, (err, results) => {
     if (err) res.status(500).json({ status: 500, message: err.sqlMessage });
     else {
