d40ac1e8260b2ff68d7109e8b14a174c654f2039
Dilip Reddy <spasupuleti@ecknologic.com>
2021-04-05 15:26:32 +0530

    reduce logo size

--- config/config.js
+++ config/config.js
@@ -1,11 +1,10 @@
-
+// config.js
 const config = {
-
   db: {
-    host: '100.25.207.145',
-    dbName: 'trackingsystem',
-    user: 'root',
-    password: 'Google@19'
+    host: process.env.HOST,
+    dbName: process.env.DB_NAME,
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD
   }
 };
 
