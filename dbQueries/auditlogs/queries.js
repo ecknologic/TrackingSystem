16a5905041102480afa6760eedd32b5b7907f731
Praveen <spasupuleti@ecknologic.com>
2021-05-05 23:03:52 +0530

    Auditlogs get API

--- dbQueries/auditlogs/queries.js
+++ dbQueries/auditlogs/queries.js
@@ -4,8 +4,9 @@ const { executeGetQuery, executeGetParamsQuery, executePostOrUpdateQuery, dateCo
 const { getDeliverysByCustomerOrderId } = require('../warehouse/queries.js');
 let auditQueries = {}
 
-auditQueries.getAudits = (customerId, callback) => {
-    let query = "SELECT c.customerName,u.userName,a.description,a.createdDateTime from auditlogs a INNER JOIN usermaster u ON a.userId=u.userId INNER JOIN customerdetails c ON a.customerId=c.customerId WHERE a.customerId=" + customerId
+auditQueries.getAudits = (input, callback) => {
+    let { type, id } = input
+    let query = "SELECT c.customerName,u.userName,a.description,a.createdDateTime from auditlogs a INNER JOIN usermaster u ON a.userId=u.userId INNER JOIN customerdetails c ON a.customerId=c.customerId WHERE a.customerId=" + id
     executeGetQuery(query, callback)
 }
 
---
+++ routes/auditlogs.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const auditQueries = require('../dbQueries/auditlogs/queries');
+var router = express.Router();
+const { dbError } = require('../utils/functions');
+let departmentId, userId;
+//Middle ware that is specific to this router
+router.use(function timeLog(req, res, next) {
+    departmentId = req.headers['departmentid']
+    userId = req.headers['userid']
+    next();
+});
+
+
+
+router.get('/getAuditLogs', (req, res) => {
+    auditQueries.getAudits(req.query, (err, results) => {
+        if (err) res.status(500).json(dbError(err));
+        else res.json(results);
+    });
+});
+
+module.exports = router;
+
