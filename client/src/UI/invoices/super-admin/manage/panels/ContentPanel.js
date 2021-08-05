051fccd933c89a791d60d5f4a3e71c87ecc1fdf3
Dilip Reddy <spasupuleti@ecknologic.com>
2021-08-06 00:08:25 +0530

    remove react-to-print, add iframe for pdf

--- client/src/UI/invoices/super-admin/manage/panels/ContentPanel.js
+++ client/src/UI/invoices/super-admin/manage/panels/ContentPanel.js
@@ -1,40 +1,20 @@
-import React, { Component, Fragment, useRef } from 'react';
-import ReactToPrint from "react-to-print";
-import Scrollbars from 'react-custom-scrollbars-2';
-import ScrollUp from '../../../../../components/ScrollUp';
-import PDFViewer from '../../../../../components/PDFViewer';
-import { base64String } from '../../../../../utils/Functions';
-import CustomButton from '../../../../../components/CustomButton';
+import React from 'react';
 import Spinner from '../../../../../components/Spinner';
 import NoContent from '../../../../../components/NoContent';
+import { base64String } from '../../../../../utils/Functions';
 
-class ContentPanel extends Component {
+const ContentPanel = ({ data, isLoading }) => {
+    const { invoicePdf } = data
+    const pdf = base64String(invoicePdf?.data, 'application/pdf')
 
-    render() {
-        const { data, onPrint, isLoading } = this.props
-        const { invoiceId, invoicePdf } = data
-        const pdf = base64String(invoicePdf?.data, 'application/pdf')
-
-        return (
-            <Fragment>
-                <div className='content-header-bar'>
-                    <span className='title'>{invoiceId}</span>
-                    <div className='menu-panel'>
-                        <ReactToPrint content={() => this.componentRef} trigger={() => <CustomButton onClick={(e) => onPrint(e, pdf)} className='app-cancel-btn' text='Print' />} />
-                    </div>
-                </div>
-                <div className='content-body'>
-                    <Scrollbars renderThumbVertical={Thumb}>
-                        <ScrollUp parent='.document-viewer' dep={pdf} />
-                        {
-                            isLoading ? <NoContent content={<Spinner />} />
-                                : <PDFViewer ref={(el) => (this.componentRef = el)} pdf={pdf} />
-                        }
-                    </Scrollbars>
-                </div>
-            </Fragment>
-        )
-    }
+    return (
+        <div className='content-body'>
+            {
+                isLoading ? <NoContent content={<Spinner />} />
+                    : <iframe src={pdf} width="100%" height="100%"></iframe>
+            }
+        </div>
+    )
 }
 
 const Thumb = (props) => <div {...props} className="thumb-vertical" />
